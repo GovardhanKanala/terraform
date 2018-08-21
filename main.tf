39641568dcf966e0bafb10e11899c250854a3448
Govadhan Reddy <govardhan@Triconinfotech.com>
2018-08-21 03:47:25 -0700

    updated vpc and subnets

--- main.tf
+++ main.tf
@@ -1,12 +1,42 @@
-provider  "aws" {
+#####################################################################
+#PROVIDER
+#####################################################################
+provider "aws" {
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
-region = "us-east-1"
-
+region       = "us-east-1"
 }
 
-resource "aws_instance" "nginx" {
-ami = "ami-cfe4b2b0"
-instance_type = "t2.micro"
-key_name = "jenkins-slave"
+######################################################################
+#DATA
+######################################################################
+data "aws_availability_zones" "available" {}
+
+######################################################################
+#RESOURCE
+######################################################################
+# NETWORKING #
+resource "aws_vpc" "vpc" {
+    cidr_block              ="${var.network_address_space}"
+    enable_dns_hostnames    = "true"
+}
+
+resource "aws_internet_gateway" "igw" {
+    vpc_id                  = "${aws_vpc.vpc.id}"
+    map_public_ip_on_launch = "true"
+    availability_zone       = "{data.aws_availability_zones.available.names[0]}"
+}
+
+resource "aws_subnet" "subnet1" {
+  cidr_block                = "${var.subnet1_address_space}"
+  vpc_id                    ="${var.aws_vpc.vpc.id}"
+  map_public_ip_on_launch   = "true"
+  availability_zone         = "${data.aws.availability_zones.available.names[0]}"
+}
+
+resource "aws_subnet" "subnet2" {
+  cidr_block                = "${var.subnet2_address_space}"
+  vpc_id                    = "${var.vpc_id.vpc.id}"
+  map_public_ip_on_launch   = "true"
+  availability_zone         = "${data.aws.availability_zones.available.names[1]}"
 }
--- variables.tf
+++ variables.tf
@@ -1,7 +1,29 @@
+####################################################################
+#variables
+####################################################################
+
 variable "aws_access_key" {
   default = ""
 }
 
 variable "aws_secret_key" {
   default = ""
+}
+
+variable "private_key_path" {
+  default = ""
+}
+
+variable "key_name" {
+  default = ""
+}
+
+variable "network_address_space" {
+  default = "10.1.0.0/16"
+}
+variable "subnet1_address_space" {
+  default = "10.1.0.0/24"
+}
+variable "subnet2_address_space" {
+  default = "10.1.1.0/24"
 }
